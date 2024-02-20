ded68561a2e07566f000f4be3105d6ec7a64f649
Sunjin Jung <sunjin9904@gmail.com>
2024-02-20 11:05:40 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const API_KEY='71437b60c0f2432b9eb41a103b941320'
+let news=[]
+let PAGE_SIZE=20
+const getLatesNews=async()=>{
+    
+    const url= new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?q=${keyword}&country=kr&pageSize=${PAGE_SIZE}`)
+    
+    const respond=await fetch(url)
+    const data=await respond.json()
+    news=data.articles
+   
+    console.log('ddd',news)
+}
+getLatesNews()
