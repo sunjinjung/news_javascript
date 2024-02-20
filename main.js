6720726eb9a3a689e82312c3c99a252fce3723cc
Sunjin Jung <sunjin9904@gmail.com>
2024-02-20 11:14:33 +0900

    first commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let news=[]
 let PAGE_SIZE=20
 const getLatesNews=async()=>{
     
-    const url= new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?page=1&pageSize=20`)
+    const url= new URL(`https://magical-biscochitos-3cd9cf.netlify.app/?page=1&pageSize=20`)
     
     const respond=await fetch(url)
     const data=await respond.json()
