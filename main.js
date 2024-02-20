ecd6a179b762dcb58753edc47c613584594968e0
Sunjin Jung <sunjin9904@gmail.com>
2024-02-20 11:12:26 +0900

    first commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let news=[]
 let PAGE_SIZE=20
 const getLatesNews=async()=>{
     
-    const url= new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=71437b60c0f2432b9eb41a103b941320`)
+    const url= new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?page=1&pageSize=20`)
     
     const respond=await fetch(url)
     const data=await respond.json()
