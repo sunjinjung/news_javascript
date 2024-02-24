3767851b48436441dadf5a06e24ca06147e18805
Sunjin Jung <sunjin9904@gmail.com>
2024-02-25 04:35:44 +0900

    first commit

--- main.js
+++ main.js
@@ -5,17 +5,27 @@ menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(eve
 
 let PAGE_SIZE=20
 let url=new URL(`https://magical-biscochitos-3cd9cf.netlify.app/top-headlines`)
+let totalResults=0
+let page =1
+const pageSize=20
+const groupSize=5
+
 
 const getNew = async()=>{
     try{
+      url.searchParams.set("page",page) 
+      url.searchParams.set("pageSize",pageSize)
       const respond=await fetch(url);
+      
       const data=await respond.json();
         if(respond.status===200){
           if(data.articles.length===0){
             throw new Error("No result for this search")
           }
         newsList=data.articles;
-      render();
+        totalResults=data.totalResults
+        render();
+        paginationRender()
       }
       else{
         throw new Error(data.message)
@@ -61,7 +71,7 @@ const imgError = (image) => {
 };
 
 const render = () => {
-	const newsHtml = newsList.map(news => `<div class="row news-list">
+	const newsHtml = newsList.map((news) => `<div class="row news-list">
 		<div class="col-lg-4 news-image">
 			<img src="${news.urlToImage}" alt="뉴스 이미지" class="news-img" onerror="imgError(this)">
 		</div>
@@ -70,8 +80,7 @@ const render = () => {
 			<p>${news.description == null || news.description == "" ? "내용없음": news.description.length > 200 ? news.description.substring(0, 200) + "..." : news.description}</p>
 			<div>${news.source.name || "no source"}  ${moment(news.publishedAt).fromNow()}</div>			
 		</div>
-	</div>`
-	).join('');
+	</div>`).join('');
 
 	document.getElementById('news-board').innerHTML = newsHtml;
 
@@ -101,6 +110,33 @@ const openNav = () => {
   }
 
   getNews();
+
+  const paginationRender=()=>{
+    const pageGroup=Math.ceil(page/groupSize);
+    let lastPage= pageGroup * groupSize;
+    
+    const totalPages=Math.ceil(totalResults/pageSize)
+    const firstPage= lastPage - (groupSize-1)<=0?1: lastPage-(groupSize-1);
+
+
+    if(lastPage>totalPages) {
+      lastPage=totalPages;
+    }
+
+    let paginationHTML=``
+    for(let i=firstPage;i<=lastPage;i++){
+      paginationHTML+=`<li class="page-item ${i===page?'active':''}" onclick="MovetoPage(${i})"><a class="page-link" >${i}</a></li>`
+    }
+    document.querySelector(".pagination").innerHTML=paginationHTML
+
+
+
+  }
+  const MovetoPage=(pageNum)=>{
+    console.log("movetopage",pageNum);
+    page=pageNum;
+    getNew()
+  }
 
   //1. 버튼들에 클릭이벤트주기
   //2. 카테고리별 뉴스 가져오기
